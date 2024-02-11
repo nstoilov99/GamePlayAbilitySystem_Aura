0d9090b4a0761e458ab09b8e9aaeac767ad45196
nstoilov99 <nstoilov99@gmail.com>
2024-02-11 21:00:44 +0200

    Adding the EnhancedInputComponent and binding function to the MoveAction

--- Source/AuraARPG/Public/Player/AuraPlayerController.h
+++ Source/AuraARPG/Public/Player/AuraPlayerController.h
@@ -6,12 +6,30 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 /**
  * 
  */
+
 UCLASS()
 class AURAARPG_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+private:
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
 };
