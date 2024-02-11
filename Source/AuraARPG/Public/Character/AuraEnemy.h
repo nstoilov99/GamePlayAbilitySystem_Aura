790bbc02626a91fe92bfe1661655c63f19de68b2
nstoilov99 <nstoilov99@gmail.com>
2024-02-11 22:18:08 +0200

    added EnemyInterface set AuraEnemy to inherit from it

--- Source/AuraARPG/Public/Character/AuraEnemy.h
+++ Source/AuraARPG/Public/Character/AuraEnemy.h
@@ -4,14 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURAARPG_API AAuraEnemy : public AAuraCharacterBase
+class AURAARPG_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
 };
