8424839e398104b79094604d332f1053b2baad27
nstoilov99 <nstoilov99@gmail.com>
2024-02-11 10:15:48 +0200

    adding Enhanced Input Actions and Context as well as creating a Player Controller Class

---
+++ Source/AuraARPG/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright ShutInGaming
+
+
+#include "Player/AuraPlayerController.h"
+
---
+++ Source/AuraARPG/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright ShutInGaming
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURAARPG_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
