318218daf409890ef1bada3ca5ba802303435059
nstoilov99 <nstoilov99@gmail.com>
2024-02-14 20:07:08 +0200

    created our HUD and added the overlay widget to it

---
+++ Source/AuraARPG/Public/UI/HUD/AuraHUD.h
@@ -0,0 +1,26 @@
+// Copyright ShutInGaming
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AuraHUD.generated.h"
+
+class UAuraUserWidget;
+/**
+ * 
+ */
+UCLASS()
+class AURAARPG_API AAuraHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+protected:
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
+};
