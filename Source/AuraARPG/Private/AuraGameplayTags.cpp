5796e0cab0f2f2a7a0147a756e60f12fc6154e06
nstoilov99 <nstoilov99@gmail.com>
2024-03-03 12:10:52 +0200

    Gameplay Tags Singleton

---
+++ Source/AuraARPG/Private/AuraGameplayTags.cpp
@@ -0,0 +1,12 @@
+// Copyright ShutInGaming
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("Reduces damage taken, improves Block Chance"));
+}
