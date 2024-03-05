3ecedb1694b1293dfe7c2e45bd19e90a5882f833
nstoilov99 <nstoilov99@gmail.com>
2024-03-05 20:37:25 +0200

    responding to attribute changes

--- Source/AuraARPG/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/AuraARPG/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -8,7 +8,19 @@
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
-	 
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	check(AttributeInfo);
+
+	for (auto& Pair : AS->TagsToAttributes)
+	{
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Pair.Value()).AddLambda(
+			[this, Pair](const FOnAttributeChangeData& Data)
+			{
+				BroadcastAttributeInfo(Pair.Key, Pair.Value());
+			}
+		);
+	}
 }
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
@@ -19,8 +31,13 @@ void UAttributeMenuWidgetController::BroadcastInitialValues()
 
 	for (auto& Pair : AS->TagsToAttributes)
 	{
-		FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(Pair.Key);
-		Info.AttributeValue = Pair.Value().GetNumericValue(AS);
-		AttributeInfoDelegate.Broadcast(Info);
+		BroadcastAttributeInfo(Pair.Key, Pair.Value());
 	}
+}
+
+void UAttributeMenuWidgetController::BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const
+{
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(AttributeTag);
+	Info.AttributeValue = Attribute.GetNumericValue(AttributeSet);
+	AttributeInfoDelegate.Broadcast(Info);
 }
--- Source/AuraARPG/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/AuraARPG/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -8,6 +8,9 @@
 
 class UAttributeInfo;
 struct FAuraAttributeInfo;
+struct FGameplayAttribute;
+struct FGameplayTag;
+
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FAuraAttributeInfo&, Info);
 
 /**
@@ -28,4 +31,7 @@ public:
 protected:
 	UPROPERTY(EditDefaultsOnly)
 	TObjectPtr<UAttributeInfo> AttributeInfo;
+
+private:
+	void BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const;
 };
