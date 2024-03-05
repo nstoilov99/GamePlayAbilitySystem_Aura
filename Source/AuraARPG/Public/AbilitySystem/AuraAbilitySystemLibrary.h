391145c9dd1ce8d191a71112261fdcbe8b30e66f
nstoilov99 <nstoilov99@gmail.com>
2024-03-05 13:34:46 +0200

    created our own blueprint function library so we can expose hidden gems in c++ to the blueprint side

---
+++ Source/AuraARPG/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,22 @@
+// Copyright ShutInGaming
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+class UOverlayWidgetController;
+/**
+ * 
+ */
+UCLASS()
+class AURAARPG_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFUNCTION(BlueprintPure, Category = "AuraAbilitySystemLibrary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
