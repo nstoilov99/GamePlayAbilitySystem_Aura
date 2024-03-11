7c9db772a932624e453eb8b9bdb79674b7fc1041
nstoilov99 <nstoilov99@gmail.com>
2024-03-11 13:14:24 +0200

    spwaning fireball with our new Gameplay Ability but it has more things that must be implemented

---
+++ Source/AuraARPG/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -0,0 +1,24 @@
+// Copyright ShutInGaming
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraGameplayAbility.h"
+#include "AuraProjectileSpell.generated.h"
+
+class AAuraProjectile;
+/**
+ * 
+ */
+UCLASS()
+class AURAARPG_API UAuraProjectileSpell : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+	
+protected:
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<AAuraProjectile> ProjectileClass;
+};
