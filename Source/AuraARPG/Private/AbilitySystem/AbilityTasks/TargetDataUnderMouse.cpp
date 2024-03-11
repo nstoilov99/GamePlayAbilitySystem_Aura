8ff905c3ee75923553bac795edbd428fa11a78b3
nstoilov99 <nstoilov99@gmail.com>
2024-03-11 20:02:32 +0200

    created target data Ability Task

---
+++ Source/AuraARPG/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -0,0 +1,21 @@
+// Copyright ShutInGaming
+
+
+#include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+
+	return MyObj;
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	FHitResult CursorHit;
+
+	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+
+	ValidData.Broadcast(CursorHit.ImpactPoint);
+}
