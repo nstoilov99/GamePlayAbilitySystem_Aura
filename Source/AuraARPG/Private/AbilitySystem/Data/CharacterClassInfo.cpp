1107393361c5d324c90dff94cbb0407555d177d1
nstoilov99 <nstoilov99@gmail.com>
2024-03-15 11:31:20 +0200

    created the Gameplay Effects to initialize the default Attributes for Enemies

---
+++ Source/AuraARPG/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright ShutInGaming
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
