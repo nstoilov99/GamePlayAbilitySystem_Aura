c15f2a35b61a6fec2a9207cb235b273310cefa2c
nstoilov99 <nstoilov99@gmail.com>
2024-03-17 03:01:03 +0200

    Enemy Death

--- Source/AuraARPG/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/AuraARPG/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -7,6 +7,7 @@
 #include "GameplayEffectExtension.h"
 #include "Net/UnrealNetwork.h"
 #include "AuraGameplayTags.h"
+#include "Interaction/CombatInterface.h"
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
@@ -136,7 +137,15 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 			SetHealth(FMath::Clamp(NewHealth, 0, GetMaxHealth()));
 
 			const bool bFatal = NewHealth <= 0;
-			if (!bFatal)
+			if (bFatal)
+			{
+				ICombatInterface* CombatInterface = Cast<ICombatInterface>(Props.TargetAvatarActor);
+				if (CombatInterface)
+				{
+					CombatInterface->Die();
+				}
+			}
+			else
 			{
 				FGameplayTagContainer TagContainer;
 				TagContainer.AddTag(FAuraGameplayTags::Get().Effects_HitReact);
--- Source/AuraARPG/Private/Character/AuraEnemy.cpp
+++ Source/AuraARPG/Private/Character/AuraEnemy.cpp
@@ -98,3 +98,9 @@ int32 AAuraEnemy::GetPlayerLevel()
 {
 	return Level;
 }
+
+void AAuraEnemy::Die()
+{
+	SetLifeSpan(LifeSpan);
+	Super::Die();
+}
