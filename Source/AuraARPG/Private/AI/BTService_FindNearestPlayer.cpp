2f7eb3fac90bbcb2d5621473e70772c33e4c8cca
nstoilov99 <nstoilov99@gmail.com>
2024-03-27 19:47:35 +0200

    created a Behavior Tree Service to find the nearest player

---
+++ Source/AuraARPG/Private/AI/BTService_FindNearestPlayer.cpp
@@ -0,0 +1,20 @@
+// Copyright ShutInGaming
+
+
+#include "AI/BTService_FindNearestPlayer.h"
+#include "AIController.h"
+#include "Kismet/GameplayStatics.h"
+
+void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	APawn* OwningPawn = AIOwner->GetPawn();
+
+	const FName TargetTag = OwningPawn->ActorHasTag(FName("Player")) ? FName("Enemy") : FName("Player");
+
+	TArray<AActor*> ActorsWithTag;
+	UGameplayStatics::GetAllActorsWithTag(this, TargetTag, ActorsWithTag);
+
+
+}
