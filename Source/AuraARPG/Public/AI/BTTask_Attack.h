2ede357e27c26661509f1ef08e0d936a3c4ceed8
nstoilov99 <nstoilov99@gmail.com>
2024-03-30 02:27:19 +0200

    created a Behavior Tree task for Attacking

---
+++ Source/AuraARPG/Public/AI/BTTask_Attack.h
@@ -0,0 +1,18 @@
+// Copyright ShutInGaming
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlueprintBase.h"
+#include "BTTask_Attack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURAARPG_API UBTTask_Attack : public UBTTask_BlueprintBase
+{
+	GENERATED_BODY()
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
