ee3f10ca05669c0e52905b7dbb3ccee8596469ce
nstoilov99 <nstoilov99@gmail.com>
2024-04-03 11:53:04 +0300

    Gameplay Tags for the Montage Events

--- Source/AuraARPG/Private/AuraGameplayTags.cpp
+++ Source/AuraARPG/Private/AuraGameplayTags.cpp
@@ -194,4 +194,23 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FName("Abilities.Attack"),
 		FString("Attack Ability Tag")
 	);
+
+	/*
+	* Abilities
+	*/
+
+	GameplayTags.Montage_Attack_Weapon = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Montage.Attack.Weapon"),
+		FString("Weapon")
+	);
+
+	GameplayTags.Montage_Attack_RightHand = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Montage.Attack.RightHand"),
+		FString("Right Hand")
+	);
+
+	GameplayTags.Montage_Attack_LeftHand = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Montage.Attack.LeftHand"),
+		FString("Left Hand")
+	);
 }
--- Source/AuraARPG/Public/AuraGameplayTags.h
+++ Source/AuraARPG/Public/AuraGameplayTags.h
@@ -52,6 +52,10 @@ public:
 	FGameplayTag Damage_Physical;
 
 	FGameplayTag Abilities_Attack;
+	
+	FGameplayTag Montage_Attack_Weapon;
+	FGameplayTag Montage_Attack_RightHand;
+	FGameplayTag Montage_Attack_LeftHand;
 
 	TMap<FGameplayTag, FGameplayTag> DamageTypesToResistances;
 
