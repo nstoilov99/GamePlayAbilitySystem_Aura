e732301fe96b5b06b9efd0dc15e062a1992a8d08
nstoilov99 <nstoilov99@gmail.com>
2024-04-07 16:08:53 +0300

    created a Blackboard key so that we can know when a Enemy is dead in the Behavior Tree

--- Source/AuraARPG/Private/Character/AuraEnemy.cpp
+++ Source/AuraARPG/Private/Character/AuraEnemy.cpp
@@ -144,5 +144,10 @@ int32 AAuraEnemy::GetPlayerLevel()
 void AAuraEnemy::Die()
 {
 	SetLifeSpan(LifeSpan);
+	if (AuraAIController)
+	{
+		AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("Dead"), true);
+	}
+
 	Super::Die();
 }
