c59734b541396db733bdfcad2f581df62ab10d7e
nstoilov99 <nstoilov99@gmail.com>
2024-04-10 23:15:36 +0300

    introduce the concept of Socket Tags to Tagged Montage in the gameplay cue MeleeImpact

--- Source/AuraARPG/Public/Interaction/CombatInterface.h
+++ Source/AuraARPG/Public/Interaction/CombatInterface.h
@@ -22,6 +22,9 @@ struct FTaggedMontage
 	FGameplayTag MontageTag;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag SocketTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	USoundBase* ImpactSound = nullptr;
 };
 // This class does not need to be modified.
@@ -64,4 +67,7 @@ public:
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	UNiagaraSystem* GetBloodEffect();
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	FTaggedMontage GetTaggedMontageByTag(const FGameplayTag& MontageTag);
 };
