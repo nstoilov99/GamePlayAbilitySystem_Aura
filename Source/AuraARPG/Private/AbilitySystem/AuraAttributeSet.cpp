b1f76689742311bb36fd227bbecd62b4092f861c
nstoilov99 <nstoilov99@gmail.com>
2024-04-29 23:36:51 +0300

    created IncomingXP Meta Attribute to handle incoming experience points

--- Source/AuraARPG/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/AuraARPG/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -11,6 +11,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "Player/AuraPlayerController.h"
 #include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "AuraLogChannels.h"
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
@@ -134,8 +135,6 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
 	{
 		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
-
-		UE_LOG(LogTemp, Warning, TEXT("Changed Health on %s, Health: %f"), *Props.TargetAvatarActor->GetName(), GetHealth());
 	}
 	if (Data.EvaluatedData.Attribute == GetManaAttribute())
 	{
@@ -170,6 +169,12 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 			const bool bCritcalHit = UAuraAbilitySystemLibrary::IsCriticalHit(Props.EffectContextHandle);
 			ShowFloatingText(Props, LocalIncomingDamage, bBlockedHit, bCritcalHit);
 		}
+	}
+	if (Data.EvaluatedData.Attribute == GetIncomingXPAttribute())
+	{
+		const float LocalIncomingXP = GetIncomingXP();
+		SetIncomingXP(0.f);
+		UE_LOG(LogAura, Warning, TEXT("Incoming XP Amount: %f"), Data.EvaluatedData.Magnitude)
 	}
 }
 
--- Source/AuraARPG/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/AuraARPG/Public/AbilitySystem/AuraAttributeSet.h
@@ -182,6 +182,10 @@ public:
 	FGameplayAttributeData IncomingDamage;
 	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, IncomingDamage);
 
+	UPROPERTY(BlueprintReadOnly, Category = "Meta Attributes")
+	FGameplayAttributeData IncomingXP;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, IncomingXP);
+
 	//REPLICATED FUNCTIONS
 	UFUNCTION()
 	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
