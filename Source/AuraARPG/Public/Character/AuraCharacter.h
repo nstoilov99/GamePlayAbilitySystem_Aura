ac13eb6ec2ed47a6fa0b3834f93c5d5cf9dd7dd3
nstoilov99 <nstoilov99@gmail.com>
2024-05-06 13:46:30 +0300

    added Niagara Effect for when the player levels up

--- Source/AuraARPG/Public/Character/AuraCharacter.h
+++ Source/AuraARPG/Public/Character/AuraCharacter.h
@@ -7,6 +7,9 @@
 #include "Interaction/PlayerInterface.h"
 #include "AuraCharacter.generated.h"
 
+class UNiagaraComponent;
+class UCameraComponent;
+class USpringArmComponent;
 /**
  * 
  */
@@ -37,6 +40,19 @@ public:
 	virtual int32 GetPlayerLevel_Implementation() override;
 	/**end Combat Interface */
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UNiagaraComponent> LevelUpNiagaraComponent;
+
 private:
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UCameraComponent> TopDownCameraComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USpringArmComponent> CameraBoom;
+
 	virtual void InitAbilityActorInfo() override;
+
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastLevelUpParticles() const;
 };
