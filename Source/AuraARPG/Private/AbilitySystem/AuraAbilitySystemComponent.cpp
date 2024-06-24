b0548d7a6e193ccafa021fe754392271b5bb5546
nstoilov99 <nstoilov99@gmail.com>
2024-06-24 16:49:03 +0300

    Showing Abilities in the Spell Tree

--- Source/AuraARPG/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/AuraARPG/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -20,7 +20,7 @@ void UAuraAbilitySystemComponent::AddCharacterAbilities(const TArray<TSubclassOf
 		if (const UAuraGameplayAbility* AuraAbility = Cast<UAuraGameplayAbility>(AbilitySpec.Ability))
 		{
 			AbilitySpec.DynamicAbilityTags.AddTag(AuraAbility->StartupInputTag);
-			AbilitySpec.DynamicAbilityTags.AddTag(FAuraGameplayTags::Get().Abilities_Status_Equipped);
+			AbilitySpec.DynamicAbilityTags.AddTag(FAuraGameplayTags::Get().Abilities_Status_Eligible);
 			GiveAbility(AbilitySpec);
 		}
 
