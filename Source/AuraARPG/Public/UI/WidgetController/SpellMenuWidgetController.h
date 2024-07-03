ab8afe6774b06f357251cee6a77dce6894ba9d84
nstoilov99 <nstoilov99@gmail.com>
2024-07-03 14:45:49 +0300

    Self Deselecting Spell Globes

--- Source/AuraARPG/Public/UI/WidgetController/SpellMenuWidgetController.h
+++ Source/AuraARPG/Public/UI/WidgetController/SpellMenuWidgetController.h
@@ -40,6 +40,9 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void SpendPointButtonPressed();
 
+	UFUNCTION(BlueprintCallable)
+	void GlobeDeselect();
+
 private:
 	static void ShouldEnableButtons(const FGameplayTag& AbilityStatus, int32 SpellPoints, bool& bShouldEnableSpellPointsButton, bool& bShouldEnableEquipButton);
 	FSelectedAbility SelectedAbility = { FAuraGameplayTags::Get().Abilities_None, FAuraGameplayTags::Get().Abilities_Status_Locked };
