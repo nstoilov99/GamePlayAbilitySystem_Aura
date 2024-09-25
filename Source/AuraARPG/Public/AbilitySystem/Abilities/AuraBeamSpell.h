2560581d02e6ddd0b58fba990a2ff4ebef916d3d
nstoilov99 <nstoilov99@gmail.com>
2024-09-25 18:28:38 +0300

    Tracing for the First Target in path

--- Source/AuraARPG/Public/AbilitySystem/Abilities/AuraBeamSpell.h
+++ Source/AuraARPG/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -20,6 +20,9 @@ public:
 
 	UFUNCTION(BlueprintCallable)
 	void StoreOwnerVariables();
+
+	UFUNCTION(BlueprintCallable)
+	void TraceFirstTarget(const FVector& BeamTargetLocation);
 protected:
 
 	UPROPERTY(BlueprintReadWrite, Category = "Beam")
