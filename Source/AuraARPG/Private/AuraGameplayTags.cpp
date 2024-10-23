534addcc4abb056efb4d227206dc534e05184e04
nstoilov99 <nstoilov99@gmail.com>
2024-10-23 17:30:12 +0300

    created The Arcane Shards Spell

--- Source/AuraARPG/Private/AuraGameplayTags.cpp
+++ Source/AuraARPG/Private/AuraGameplayTags.cpp
@@ -276,6 +276,10 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		FString("Summon Ability Tag")
 	);
 
+	/*
+	*  Offensive Spells
+	*/
+
 	GameplayTags.Abilities_Fire_FireBolt = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Abilities.Fire.Firebolt"),
 		FString("Firebolt Ability Tag")
@@ -284,6 +288,11 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.Abilities_Lightning_Electrocute = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Abilities.Lightning.Electrocute"),
 		FString("Electrocute Ability Tag")
+	);
+	
+	GameplayTags.Abilities_Arcane_ArcaneShards = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Abilities.Arcane.ArcaneShards"),
+		FString("Arcane Shards Ability Tag")
 	);
 
 	/*
