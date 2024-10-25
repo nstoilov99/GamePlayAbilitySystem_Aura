5061ccc24dbba6ad3e71196283d66879a75a0eb0
nstoilov99 <nstoilov99@gmail.com>
2024-10-25 17:03:32 +0300

    created points collection for the Arcane Shards

---
+++ Source/AuraARPG/Private/Actor/PointCollection.cpp
@@ -0,0 +1,69 @@
+// Copyright ShutInGaming
+
+
+#include "Actor/PointCollection.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+
+APointCollection::APointCollection()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	Pt_0 = CreateDefaultSubobject<USceneComponent>("Pt_0");
+	ImmutablePts.Add(Pt_0);
+	SetRootComponent(Pt_0);
+
+	CREATE_AND_SETUP_PT(Pt_1);
+	CREATE_AND_SETUP_PT(Pt_2);
+	CREATE_AND_SETUP_PT(Pt_3);
+	CREATE_AND_SETUP_PT(Pt_4);
+	CREATE_AND_SETUP_PT(Pt_5);
+	CREATE_AND_SETUP_PT(Pt_6);
+	CREATE_AND_SETUP_PT(Pt_7);
+	CREATE_AND_SETUP_PT(Pt_8);
+	CREATE_AND_SETUP_PT(Pt_9);
+	CREATE_AND_SETUP_PT(Pt_10);
+
+}
+
+TArray<USceneComponent*> APointCollection::GetGroundPoints(const FVector& GroundLocation, int32 NumPoints, float YawOverride)
+{
+	checkf(ImmutablePts.Num() >= NumPoints, TEXT("Attempted to access ImmutablePts out of bounds."));
+
+	TArray<USceneComponent*> ArrayCopy;
+
+	for (USceneComponent* Pt : ImmutablePts)
+	{
+		if (ArrayCopy.Num() >= NumPoints) return ArrayCopy;
+
+		if (Pt != Pt_0)
+		{
+			FVector ToPoint = Pt->GetComponentLocation() - Pt_0->GetComponentLocation();
+			ToPoint = ToPoint.RotateAngleAxis(YawOverride, FVector::UpVector);
+			Pt->SetWorldLocation(Pt_0->GetComponentLocation() + ToPoint);
+		}
+		const FVector RaisedLocation = FVector(Pt->GetComponentLocation().X, Pt->GetComponentLocation().Y, Pt->GetComponentLocation().Z + 500.f);
+		const FVector LoweredLocation = FVector(Pt->GetComponentLocation().X, Pt->GetComponentLocation().Y, Pt->GetComponentLocation().Z - 500.f);
+
+		FHitResult HitResult;
+		TArray<AActor*> IgnoreActors;
+		TArray<AActor*> Empty;
+		UAuraAbilitySystemLibrary::GetLivePlayersWithingRadius(this, IgnoreActors, Empty, 1500.f, GetActorLocation());
+		 
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActors(IgnoreActors);
+		GetWorld()->LineTraceSingleByProfile(HitResult, RaisedLocation, LoweredLocation, FName("BlockAll"), QueryParams);
+		const FVector AdjustedLocation = FVector(Pt->GetComponentLocation().X, Pt->GetComponentLocation().Y, HitResult.ImpactPoint.Z);
+		Pt->SetWorldLocation(AdjustedLocation);
+		Pt->SetWorldRotation(UKismetMathLibrary::MakeRotFromZ(HitResult.ImpactNormal));
+
+		ArrayCopy.Add(Pt);
+	}
+
+	return ArrayCopy;
+}
+
+void APointCollection::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
