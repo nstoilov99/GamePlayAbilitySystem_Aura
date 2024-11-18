354f6b2c75a8a2c5363562297edf456d246d9511
nstoilov99 <nstoilov99@gmail.com>
2024-11-19 00:58:42 +0200

    constructing the View Model

--- Source/AuraARPG/Public/UI/HUD/LoadScreenHUD.h
+++ Source/AuraARPG/Public/UI/HUD/LoadScreenHUD.h
@@ -6,6 +6,8 @@
 #include "GameFramework/HUD.h"
 #include "LoadScreenHUD.generated.h"
 
+class ULoadScreenWidget;
+class UMVVM_LoadScreen;
 /**
  * 
  */
@@ -14,4 +16,19 @@ class AURAARPG_API ALoadScreenHUD : public AHUD
 {
 	GENERATED_BODY()
 	
+public:
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> LoadScreenWidgetClass;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<ULoadScreenWidget> LoadScreenWidget;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UMVVM_LoadScreen> LoadScreenViewModelClass;
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UMVVM_LoadScreen> LoadScreenViewModel;
+protected:
+	virtual void BeginPlay() override;
 };
